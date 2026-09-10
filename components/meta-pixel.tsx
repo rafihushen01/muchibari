f9030469e2a96687c0ba99ba9aba257d112b8fcc
Md Rafi Khan <movieanime641@gmail.com>
2026-09-10 12:39:45 +0600

    Add frontend confirmation message to MetaPixel
    Added a new frontend confirmation message to the MetaPixel component.

--- components/meta-pixel.tsx
+++ components/meta-pixel.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from 'react'
 import { usePathname } from 'next/navigation'
 import { initMetaPixel, trackPageView } from '@/lib/analytics/meta'
-
+// new frontend confirmation message
 export function MetaPixel() {
   const pathname = usePathname()
 
